77faee8dc2496936058dd0e347e4206d96ea0674
paulbrenet <brenetpaul@gmail.com>
2023-07-06 00:59:40 +0200

    Not tested: complete implementation of the intensities for all lights, for n persons

--- Object_detection_using_video/Ambiancer.py
+++ Object_detection_using_video/Ambiancer.py
@@ -82,8 +82,8 @@ class Ambiancer(object):
 
     def update_ambiance(self, persons_pos):
         self.convert_rel_light_pos()
-        lights_intensity, sum_intensities = self.fill_light_intensity(persons_pos)
-        self.compute_lights_intensity(lights_intensity, sum_intensities)
+        intensities_lights_all_persons = self.fill_light_intensity(persons_pos)
+        #self.compute_lights_intensity_per_person(lights_intensity_per_person, sum_intensities_per_person)
         self.power_intensity()
 
     def distance_person_to_light(self, light, person_pos):
@@ -99,35 +99,55 @@ class Ambiancer(object):
 
     def fill_light_intensity(self, persons_pos):
 
-        lights_intensity = {}
-        sum_intensities = 0
+        lights_intensity_per_person = {}
+        lights_intensity_norm = {}
+        distances_light_person = []
+        Itotal_person = []
+        Itotal_person_sum = 0
 
-        for light in self.lights:
-            light_intensity = []
-            for person_pos in persons_pos:
-                light_intensity.append(self.distance_person_to_light(light, person_pos))
+        intensities_lights_all_persons = {}
+        n_person = len(persons_pos)
 
-            sum_intensities = sum_intensities + sum(light_intensity)
+        for person_pos in persons_pos:
+            #create the list of all intensities from person to all lights
+            for light in self.lights:
+                distances_light_person.append(self.distance_person_to_light(light, person_pos))
 
-            lights_intensity[light.get_id()] = sum(light_intensity)
+            # Ia = S/A
+            for light in self.lights:
+                lights_intensity_per_person[light.get_id()] = int(sum(distances_light_person)/self.distance_person_to_light(light, person_pos))
+                Itotal_person.append(lights_intensity_per_person[light.get_id()])
 
-        print("this is the intensity !!!!")
-        print(sum_intensities)
-        return lights_intensity, sum_intensities
+            Itotal_person_sum = sum(Itotal_person)  # maybe if dict returns the 2 values of the 2 columns, just grab the second one
+            #normalize this intensity sum I = 1
+            for light in self.lights:
+                #vector id_light: list of intensities normalized for all persons fed one by one
+                lights_intensity_norm[light.get_id()].append(int(lights_intensity_per_person[light.get_id()]/Itotal_person_sum))
+
+            for light in self.lights:
+                # N personnes, intensity of a light is the average of all intensities
+                intensities_lights_all_persons[light.get_id()] = int(sum(lights_intensity_norm[light.get_id()])/n_person)
+
+        return intensities_lights_all_persons
 
     def get_light_by_id(self, id):
         for light in self.lights:
             if light.get_id() == id:
                 return light
 
-    def compute_lights_intensity(self, lights_intensity, sum_intensities):
+    def compute_lights_intensity_per_person(self, intensities_lights_all_persons):
         print("lights intensities")
-        print(lights_intensity)
-        print(type(lights_intensity))
-        for key_id in lights_intensity:
-            value_light_intensity = lights_intensity[key_id]
+        print(intensities_lights_all_persons)
+        print(type(intensities_lights_all_persons))
+        for key_id in intensities_lights_all_persons:
+            value_light_intensity = intensities_lights_all_persons[key_id]
             light = self.get_light_by_id(key_id)
-            light.udpate_intensity(int(value_light_intensity/sum_intensities*100))
+            light.udpate_intensity(value_light_intensity)  #see calculation, Ia = S/A, then normalize
+
+        #normalize
+        #for key_id in lights_intensity_per_person:
+
+
 
     def power_intensity(self):
         for light in self.lights:
